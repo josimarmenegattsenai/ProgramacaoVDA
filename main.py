60aae077ef21d7968f4b193d4aac48428c534191
josimarmenegattsenai <josimar.menegatt@edu.sc.senai.br>
2025-05-09 23:03:26 +0000

    update

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
+import os
 
-app = Flask(__name__)
+# Define a pasta onde estão os arquivos HTML (neste caso, a raiz do projeto)
+template_dir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__, template_folder=template_dir)
 
 @app.route("/")
 def home():
